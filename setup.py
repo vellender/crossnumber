385671deb693dbf1b00cac4e5f1f9ca62dbc06b3
vellender <adam@vellender.com>
2020-01-10 21:55:16 +0000

    Added requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'crossnumber',
   packages = ['crossnumber'], # this must be the same as the name above
+  install_requires=['numpy', 'termcolor'],
   version = '0.01',
   description = 'A collection of Python functions which may be of use when solving crossnumber puzzles.',
   author = 'Adam Vellender',
   author_email = 'dev@vellender.com',
   url = 'https://github.com/vellender/crossnumber',
   download_url = 'https://github.com/vellender/crossnumber/tarball/0.01',
-  keywords = ['crossnumber','solver','solve','tools','tool','puzzle'], 
+  keywords = ['crossnumber','solver','solve','tools','tool','puzzle','listener','magpie','numerical','numericals','crossword','mathematical','mathematicals'], 
   classifiers = [],
 )
