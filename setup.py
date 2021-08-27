464e08c85a2d07498bab92157c3829fc541c0caa
vellender <adam@vellender.com>
2021-08-27 13:42:04 +0100

    v0.04

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
   name = 'crossnumber',
   packages = ['crossnumber'], # this must be the same as the name above
   install_requires=['numpy', 'termcolor','sympy'],
-  version = '0.03',
+  version = '0.04',
   description = 'A collection of Python functions which may be of use when solving crossnumber puzzles.',
   author = 'Adam Vellender',
   author_email = 'dev@vellender.com',
   url = 'https://github.com/vellender/crossnumber',
-  download_url = 'https://github.com/vellender/crossnumber/tarball/0.03',
+  download_url = 'https://github.com/vellender/crossnumber/tarball/0.04',
   keywords = ['crossnumber','solver','solve','tools','tool','puzzle','listener','magpie','numerical','numericals','crossword','mathematical','mathematicals'], 
   classifiers = [],
 )
